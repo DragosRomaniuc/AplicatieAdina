07a72b1cdf2273a646aff140a4fd73e471cfade0
Dragos Romaniuc <dragos.romaniuc@3pillarglobal.com>
2020-04-12 10:39:15 +0300

    12 aprilie

--- backend/routes/account/index.js
+++ backend/routes/account/index.js
@@ -6,7 +6,7 @@ const { Account } = require('../../models');
 router.post('/createAccount', async (request, response, next) => {
     const { username, password } = request.body;
     // username echivalent cu request.body.username
-    
+
     const createdAccount = new Account({  //dureaza 3 secunde si e asincrona
         username,
         password
@@ -19,9 +19,6 @@ router.post('/createAccount', async (request, response, next) => {
         })
      })
 
-    
-    
-        
    
 });
 
